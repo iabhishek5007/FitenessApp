48baf40e12cb624ce21e10e93ec6036d8cccff53
Abhishek Kumar <iabhishek5007@gmail.com>
2021-01-03 23:26:00 +0530

    Initial commit

---
+++ app/src/main/java/com/rckdeveloper/fitenessapp/SkillsTrainingActivity.java
@@ -0,0 +1,14 @@
+package com.rckdeveloper.fitenessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SkillsTrainingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skills_training);
+    }
+}
